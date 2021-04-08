b458bc1bc962c0038f341e71d48f47ca5cd8e63c
Adam Skrášek <469294@muni.cz>
2021-04-08 15:54:21 +0200

    Basic storage on card
    - Store byte permanently on card and send it back + test
    - Light code cleanup, though its still really bad

--- applet/src/main/java/applet/MainApplet.java
+++ applet/src/main/java/applet/MainApplet.java
@@ -7,32 +7,34 @@ import javacardx.crypto.*;
 
 public class MainApplet extends Applet implements MultiSelectable {
     final static byte CLA_MAINAPPLET = (byte) 0xB0;
-    private static final short BUFFER_SIZE = 32;
 
-    private byte[] tmpBuffer = JCSystem.makeTransientByteArray(BUFFER_SIZE, JCSystem.CLEAR_ON_DESELECT);
-    private RandomData random;
+
     final static byte INS_TEST = (byte) 0x50;
     final static byte INS_RECEIVE = (byte) 0x51;
-
+    final static byte INS_GET_KEYLEN = (byte) 0x70;
 
     private byte m_ramArray[] = null;
 
+    private byte KeyLenArray[] = new byte[4];
+
     //Exceptions
-    final static short SW_WRONG_DATA = (short) 0x6A80 ;
+    final static short SW_WRONG_DATA = (short) 0x6A80;
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new MainApplet(bArray, bOffset, bLength);
     }
 
     public MainApplet(byte[] buffer, short offset, byte length) {
-        random = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
+
+
         m_ramArray = JCSystem.makeTransientByteArray((short) 260, JCSystem.CLEAR_ON_DESELECT);
         register();
+
     }
 
     public void process(APDU apdu) {
 
-		byte[] apduBuffer = apdu.getBuffer();
+        byte[] apduBuffer = apdu.getBuffer();
 
         // ignore the applet select command dispached to the process
         if (selectingApplet()) {
@@ -48,6 +50,10 @@ public class MainApplet extends Applet implements MultiSelectable {
                         break;
                     case INS_RECEIVE:
                         ParsePsbt(apdu);
+                        break;
+
+                    case INS_GET_KEYLEN:
+                        SendKeyLen(apdu);
                         break;
                     default:
                         // The INS code is not supported by the dispatcher
@@ -66,29 +72,69 @@ public class MainApplet extends Applet implements MultiSelectable {
     }
 
     void Test(APDU apdu) {
-    	byte[] apdubuf = apdu.getBuffer();
-    	short randomDataLen = (short) 42069;
-		apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, randomDataLen);
+        byte[] apdubuf = apdu.getBuffer();
+        short randomDataLen = (short) 20;
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, randomDataLen);
 
-	}
+    }
 
-	void ParsePsbt(APDU apdu) throws  ISOException{
+    void ParsePsbt(APDU apdu) throws ISOException {
+// Part of valid PSBT 70736274ff0100750200000001268171371edff285e937adeea4b37b78000c0566cbb3ad64641713ca42171bf60000000000feffffff02d3dff50
 //Using the ram array is more of an prototype experiment, then the future go to variant.
         byte[] apdubuf = apdu.getBuffer();
         //copy data to local ram part
-        Util.arrayCopyNonAtomic(apdubuf, (short) 5, m_ramArray, (short) 0, apdubuf[ISO7816.OFFSET_LC] );
+        Util.arrayCopyNonAtomic(apdubuf, (short) 5, m_ramArray, (short) 0, apdubuf[ISO7816.OFFSET_LC]);
 
         //copy half of input data from ram to outgoing packet
         Util.arrayCopyNonAtomic(m_ramArray, (short) 0, apdubuf, ISO7816.OFFSET_CDATA, (short) 5);
-        byte[] magic = {0x70, 0x73 ,0x62, 0x74 , (byte) 0xFF};
+        byte[] magic = {0x70, 0x73, 0x62, 0x74, (byte) 0xFF};
 
         //check magic bytes
-        for(short i =0; i <5 ;i++) {
+        for (short i = 0; i < 5; i++) {
             if (magic[i] != m_ramArray[i]) {
                 ISOException.throwIt(ISO7816.SW_WRONG_DATA);
             }
         }
+        //Global map
+        byte key_lengthb = m_ramArray[6];
+        byte keylen = 0;
+
+        //TODO:Handle the parsing
+        switch (key_lengthb) {
+            case (byte) 0xfd: {
+                byte[] buf = new byte[2];
+                //Next two bytes
+            }
+            case (byte) 0xfe: {
+                byte[] buf = new byte[4];
+                //next four bytes
+
+            }
+            case (byte) 0xff: {
+                byte[] buf = new byte[8];
+                //next 8 bytes
+            }
+            default:
+//               //just use they byte
+                keylen = key_lengthb;
+        }
+        Util.arrayFillNonAtomic(KeyLenArray, (short) 0, (short)1, keylen);
         apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short) 5);
+    }
+
+    private byte[] parse_key() {
+
+        //TODO:
+        return new byte[1];
+    }
+
+     void SendKeyLen(APDU apdu) {
+        //TODO:error handling, dynamic value
+        byte[] apdubuf = apdu.getBuffer();
+        Util.arrayCopyNonAtomic(KeyLenArray, (short) 0, apdubuf, ISO7816.OFFSET_CDATA, (short) 1);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short) 5);
+    }
+
 
 /*
  <psbt> := <magic> <global-map> <input-map>* <output-map>*
@@ -101,7 +147,7 @@ public class MainApplet extends Applet implements MultiSelectable {
  <value> := <valuelen> <valuedata>
  */
 
-    }
+
     public boolean select(boolean b) {
         return true;
     }
--- applet/src/test/java/tests/AppletTest.java
+++ applet/src/test/java/tests/AppletTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import cardTools.CardManager;
 import cardTools.CardType;
 import cardTools.RunConfig;
 import org.junit.Assert;
@@ -49,11 +50,33 @@ public class AppletTest extends BaseTest {
         Assert.assertEquals(0x9000, responseAPDU.getSW());
         Assert.assertNotNull(responseAPDU.getBytes());*/
 
+        //TODO: Correct test formats
+        //Only one connect, so it does not reset cards data
+        final CardManager manager = connect();
+
         byte[] test = {0x70, 0x73, 0x62, 0x74, (byte) 0xFF, 0x6d, 0x61, 0x72, 0x63, 0x6f};
         final CommandAPDU cmd = new CommandAPDU(0xB0, 0x51, 0,0, test);
+        final ResponseAPDU response = manager.transmit(cmd);
+        Assert.assertEquals(0x9000, response.getSW());
+        byte[] res = response.getData();
+
+
+        //retrieve
+        final CommandAPDU cmd2 = new CommandAPDU(0xB0, 0x70, 0, 0);
+        final ResponseAPDU response2 = manager.transmit(cmd2);
+        Assert.assertEquals(0x9000, response2.getSW());
+        byte[] res2 = response2.getData();
+        Assert.assertEquals((byte) 0x61, res2[0]);
+    }
+
+    /*@Test
+    public void TestDataRetrieve() throws Exception{
+        byte[] test = {0x70, 0x73, 0x62, 0x74, (byte) 0xFF, 0x01, 0x02,0x03};
+        final CommandAPDU cmd = new CommandAPDU(0xB0, 0x70, 0, 0, test);
         final ResponseAPDU response = connect().transmit(cmd);
         Assert.assertEquals(0x9000, response.getSW());
         byte[] res = response.getData();
-        int c = 2;
-    }
+        Assert.assertEquals(res[0], (byte) 0x20);
+
+    }*/
 }
